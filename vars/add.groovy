c67499dd0402bc0591e0f8deb91652f62fe72b8c
varadevops32 <varadevops6@gmail.com>
2025-06-04 15:46:50 +0530

    Update add.groovy

--- vars/add.groovy
+++ vars/add.groovy
@@ -1,3 +1,12 @@
 def call(x,y){
-  echo "Sum of $x and $y is "
+  echo "Sum of $x and $y is ${x+y} "
+}
+def call(x,y){
+  echo "sub of $x and $y is ${x-y} "
+}
+def call(x,y){
+  echo "mul of $x and $y is ${x*y} "
+}
+def call(x,y){
+  echo "div of $x and $y is ${x/y} "
 }
